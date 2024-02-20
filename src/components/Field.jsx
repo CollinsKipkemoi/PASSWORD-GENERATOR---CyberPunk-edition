7e75f1e685cb18e8777ae85b98daf6760fad8e62
Byte Me <chemerilcollins@gmail.com>
2024-02-20 11:31:41 +0100

    Password generation logic

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,19 +1,47 @@
 import "../App.css"
 import { useState } from "react"
+import zxcvbn from 'zxcvbn';
+import securePassword from 'secure-random-password';
+
+
 function Field() {
     const [length, setLength] = useState(8)
+    const [generatedPassword, setGeneratedPassword] = useState('')
+    const [passwords, setPasswords] = useState([]);
     const handleSlide = (e) => {
         setLength(parseInt(e.target.value))
     }
-    const[passwords, setPasswords] = useState([]);
+
+    const generate = () => {
+        const uppercase = document.getElementById('uppercase').checked;
+        const lowercase = document.getElementById('lowercase').checked;
+        const numbers = document.getElementById('numbers').checked;
+        const symbols = document.getElementById('symbols').checked;
+        const characters = [
+            lowercase ? securePassword.lower : '',
+            uppercase ? securePassword.upper : '',
+            numbers ? securePassword.digits : '',
+            symbols ? securePassword.symbols : '',
+        ].filter(Boolean); 
+        const password = securePassword.randomPassword({
+            length: length,
+            characters: characters.length ? characters : [securePassword.lower, securePassword.upper], 
+        });
+        console.log(password);
+        const result = zxcvbn(password);
+        console.log(result.score);
+        setPasswords([...passwords, password]);
+        setGeneratedPassword(password);
+        return password;
+    }
     return (
         <div className="container container-fluid mt-2 ps-0 pe-0 field">
             <div className="container w-50 container-fluid parts">
                 {/* password field */}
-                <p className="password-field"></p>
+                <p className="password-field">{generatedPassword}</p>
                 {/* generate button */}
                 <div className="buttons">
-                    <button className="btn btn-sm generate-btn">Generate</button>
+                    <button className="btn btn-sm generate-btn" onClick={generate}>Generate</button>
                     {/* copy button */}
                     <button className="btn btn-sm copy-btn">Copy</button>
                 </div>
