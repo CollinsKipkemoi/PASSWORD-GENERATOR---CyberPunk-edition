07b372081a6eee720d529fbd366f7a21a8b04e9b
Byte Me <chemerilcollins@gmail.com>
2024-02-22 09:01:10 +0100

    Custom range + minor bug fixes.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
   return (
-    <div className="container container-fluid header">
+    <div className="container container-fluid mb-5 header">
         <h1>PASSWORD </h1>  
         <h1>GENERATOR</h1>    
     </div>
